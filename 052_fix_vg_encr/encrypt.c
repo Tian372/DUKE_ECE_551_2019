1bfd9b230ea3ae062996bb511c6b88dfcb7fc83e
zt45 <zt45@duke.edu>
2019-09-27 03:31:02 -0400

    ;p!

--- 052_fix_vg_encr/encrypt.c
+++ 052_fix_vg_encr/encrypt.c
@@ -4,8 +4,8 @@
 #include <string.h>
 
 void encrypt(FILE * f, int key, FILE * outfile) {
-  char * line;
-  size_t sz;
+  char * line = 0;
+  size_t sz = 0;
   while (getline(&line, &sz, f) >= 0) {
     char * ptr = line;
     while (*ptr != '\0') {
@@ -40,8 +40,8 @@ int main(int argc, char ** argv) {
     perror("Could not open file");
     return EXIT_FAILURE;
   }
-  //outfileNAme is argv[2] + ".txt", so add 4 to its length.
-  char * outFileName = malloc((strlen(argv[2]) + 4) * sizeof(*outFileName));
+  //outfileNAme is argv[2] + ".enc", so add (4+1) to its length. (1 for \0)
+  char * outFileName = malloc((strlen(argv[2]) + 5) * sizeof(*outFileName));
   strcpy(outFileName, argv[2]);
   strcat(outFileName, ".enc");
   FILE * outFile = fopen(outFileName, "w");
@@ -58,3 +58,5 @@ int main(int argc, char ** argv) {
 
   return EXIT_SUCCESS;
 }
+
+// vim: ts=2:sw=2:et
