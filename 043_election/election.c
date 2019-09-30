2af920ca61f39a7b25593b85acef13011afed466
zt45 <zt45@duke.edu>
2019-09-30 11:20:15 -0400

    ;p!

--- 043_election/election.c
+++ 043_election/election.c
@@ -5,7 +5,12 @@
 #include <string.h>
 
 //include any other headers you need here...
-
+//struct state_tag {
+//  char name[MAX_STATE_NAME_LENGTH];
+//  uint64_t population;
+//  unsigned int electoralVotes;
+//};
+//typedef struct state_tag state_t;
 state_t parseLine(const char * line) {
   //STEP 1: write me
   char * p = strdup(line);
@@ -37,21 +42,22 @@ state_t parseLine(const char * line) {
   //converting population from string to int
   int pop = atoi(cpop);
   cnum = strsep(&p, ":");
-
+  //get the length of state names
   size_t name_len = strlen(name);
-
+  //making sure the name is within the max length
   if (name_len > MAX_STATE_NAME_LENGTH - 1) {
     printf("The name is too long");
     exit(EXIT_FAILURE);
   }
+
   int num = atoi(cnum);
   //copy from a pointer to array
   memcpy(state.name, name, name_len);
   state.name[name_len] = '\0';
-
-  state.population = pop;
+  //assign value to population and votes
+  state.population = (unsigned int)pop;
   state.electoralVotes = num;
-
+  //return state_t state
   return state;
 }
 
@@ -60,7 +66,8 @@ unsigned int countElectoralVotes(state_t * stateData,
                                  size_t nStates) {
   //STEP 2: write me
   unsigned int ev = 0;
-  //
+  //loop through stateDate, everytime voteCounts > 50% population
+  //add the electoralVotes
   for (size_t i = 0; i < nStates; i++) {
     if (voteCounts[i] > 0.5 * stateData[i].population) {
       ev = ev + stateData[i].electoralVotes;
@@ -72,10 +79,12 @@ unsigned int countElectoralVotes(state_t * stateData,
 
 void printRecounts(state_t * stateData, uint64_t * voteCounts, size_t nStates) {
   //STEP 3: write me
-  double percent = 0;
-  double pop = 0;
-  double vc = 0;
-
+  double percent = 0.0;
+  double pop = 0.0;
+  double vc = 0.0;
+  //I do not know if there will be problems casting uint64 to double
+  //see if the persentage of votes is bewteen 49.5% and 50.5%
+  //if so, recount
   for (size_t i = 0; i < nStates; i++) {
     if (voteCounts[i] > 0.495 * stateData[i].population &&
         voteCounts[i] < 0.505 * stateData[i].population) {
@@ -97,7 +106,7 @@ void printLargestWin(state_t * stateData, uint64_t * voteCounts, size_t nStates)
   double largest_p = 0.0;
 
   size_t index_L = 0;
-
+  //loop through stateDate, looking for the larget percentage
   for (size_t i = 0; i < nStates; i++) {
     pop = stateData[i].population;
     vc = voteCounts[i];
