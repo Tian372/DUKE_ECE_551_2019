1fee82917becc33ee876daebab1b4133e58fc43a
zt45 <zt45@duke.edu>
2019-10-02 11:16:25 -0400

    :p!

--- 057_outname/outname.c
+++ 057_outname/outname.c
@@ -1,8 +1,17 @@
+#include "outname.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "outname.h"
 
 char * computeOutputFileName(const char * inputName) {
   //WRITE ME
+  size_t len = strlen(inputName);
+  char * out;
+
+  out = strdup(inputName);
+  out = realloc(out, (len + 8) * sizeof(*out));
+  memcpy(out + len, ".counts", 8);
+
+  return out;
 }
