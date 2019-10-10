0f7b9b5d5db25fb396a2696b16b242e2d883cba7
zt45 <zt45@duke.edu>
2019-10-10 16:57:39 -0400

    :p!

--- 064_vector_2d/vector.cpp
+++ 064_vector_2d/vector.cpp
@@ -5,3 +5,34 @@
 
 /* write your class implementation in this file
  */
+
+void Vector2D::initVector(double init_x, double init_y) {
+  x = init_x;
+  y = init_y;
+}
+double Vector2D::getMagnitude() const {
+  double dis = 0;
+  dis = sqrt(x * x + y * y);
+  return dis;
+}
+
+Vector2D Vector2D::operator+(const Vector2D & rhs) const {
+  Vector2D sum;
+  sum.x = x + rhs.x;
+  sum.y = y + rhs.x;
+  return sum;
+}
+Vector2D & Vector2D::operator+=(const Vector2D & rhs) {
+  x += rhs.x;
+  y += rhs.y;
+  return *this;
+}
+
+double Vector2D::dot(const Vector2D & rhs) const {
+  double product;
+  product = x * rhs.x + y * rhs.y;
+  return product;
+}
+void Vector2D::print() {
+  printf("<%.2f, %.2f>", x, y);
+}
