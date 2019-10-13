4ba70423c2401a04a3ec37d3136ec12bfb72ff4b
zt45 <zt45@duke.edu>
2019-10-13 15:38:05 -0400

    ;p!

--- 060_stat/mystat.c
+++ 060_stat/mystat.c
@@ -1,22 +1,35 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <time.h>
+#include <ctype.h>
+#include <grp.h>
+#include <pwd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <pwd.h>
-#include <grp.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <time.h>
 #include <unistd.h>
 //This function is for Step 4
 char * time2str(const time_t * when, long ns) {
   char * ans = malloc(128 * sizeof(*ans));
   char temp1[64];
   char temp2[32];
-  const struct tm * t= localtime(when);
-  strftime(temp1,512,"%Y-%m-%d %H:%M:%S",t);  
-  strftime(temp2,32,"%z",t);
-  snprintf(ans,128,"%s.%09ld %s", temp1, ns, temp2);
+  const struct tm * t = localtime(when);
+  strftime(temp1, 512, "%Y-%m-%d %H:%M:%S", t);
+  strftime(temp2, 32, "%z", t);
+  snprintf(ans, 128, "%s.%09ld %s", temp1, ns, temp2);
   return ans;
 }
 
+int main(int argc, char ** argv) {
+  if (argc > 2) {
+    printf("too many arguments");
+    exit(EXIT_FAILURE);
+  }
+  if ((argv[1]) != NULL) {
+    return EXIT_FAILURE;
+  }
 
+  const char * file = argv[1];
+  printf("  File: '%s'\n", file);
+
+  return EXIT_SUCCESS;
+}
