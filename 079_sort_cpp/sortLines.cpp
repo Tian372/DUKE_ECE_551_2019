4643e979bd1445009ed0b2b7a5837ca8f540972e
zt45 <zt45@duke.edu>
2019-10-23 12:12:32 -0400

    :

--- 079_sort_cpp/sortLines.cpp
+++ 079_sort_cpp/sortLines.cpp
@@ -1,1 +1,51 @@
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <vector>
 
+int main(int argc, char ** argv) {
+  std::ifstream read;
+  std::string strings;
+
+  std::vector<std::string> str;
+
+  if (argc == 1) {
+    while (!std::cin.eof()) {
+      std::getline(std::cin, strings);
+
+      str.push_back(strings);
+
+      std::sort(str.begin(), str.end());
+
+      for (size_t i = 0; i < str.size(); i++) {
+        std::cout << str[i] << std::endl;
+      }
+
+      str.clear();
+    }
+  }
+
+  else {
+    for (int i = 1; i < argc; i++) {
+      read.open(argv[i]);
+      if (read.fail()) {
+        exit(EXIT_FAILURE);
+      }
+      while (!read.eof()) {
+        std::getline(read, strings);
+        str.push_back(strings);
+      }
+
+      std::sort(str.begin(), str.end());
+
+      for (size_t i = 0; i < str.size(); i++) {
+        std::cout << str[i] << std::endl;
+      }
+
+      str.clear();
+      read.close();
+    }
+  }
+
+  return EXIT_SUCCESS;
+}
