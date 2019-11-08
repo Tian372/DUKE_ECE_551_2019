493a2c16668786cdb904ab387bb6c6f3b22a5d63
zt45 <zt45@duke.edu>
2019-11-08 17:27:35 -0500

    :

--- 097_bst_map/bstmap.h
+++ 097_bst_map/bstmap.h
@@ -1,7 +1,6 @@
 #include <cstdio>
 #include <cstdlib>
 #include <iostream>
-#include <stdexcept>
 
 #include "map.h"
 
@@ -14,45 +13,51 @@ class BstMap : public Map<K, V> {
     V value;
     Node * left;
     Node * right;
-    Node() : key(NULL), value(NULL), left(NULL), right(NULL) {}
     Node(const K & k, const V & v) : key(k), value(v), left(NULL), right(NULL) {}
   };
   Node * root;
 
  public:
   BstMap() : root(NULL) {}
-  BstMap(const BstMap<K, V> & rhs) : root(NULL) { root = copy(rhs.root); }
+
   Node * copy(const Node * n) {
     if (n == NULL) {
       return NULL;
     }
-    Node * nextNode = new Node(n->key, n->value);
+    Node * newNode = new Node(n->key, n->value);
     if (n->left != NULL) {
-      nextNode->left = copy(n->left);
+      newNode->left = copy(n->left);
     }
     if (n->right != NULL) {
-      nextNode->right = copy(n->right);
+      newNode->right = copy(n->right);
     }
-    return nextNode;
+    return newNode;
+  }
+  BstMap(const BstMap<K, V> & rhs) : root(NULL) { root = copy(rhs.root); }
+  BstMap<K, V> & operator=(const BstMap<K, V> & rhs) {
+    if (this != &rhs) {
+      destroy(root);
+      root = copy(rhs.root);
+    }
+    return *this;
   }
 
   virtual void add(const K & key, const V & value) {
-    Node * newNode = new Node(key, value);
     Node * location = root;
-
+    Node * toAdd = new Node(key, value);
     while (location != NULL) {
-      if (location->key > key) {
+      if (key < location->key) {
         if (location->left == NULL) {
-          location->left = newNode;
+          location->left = toAdd;
           return;
         }
         else {
           location = location->left;
         }
       }
-      else if (location->key < key) {
+      else if (key > location->key) {
         if (location->right == NULL) {
-          location->right = newNode;
+          location->right = toAdd;
           return;
         }
         else {
@@ -61,104 +66,143 @@ class BstMap : public Map<K, V> {
       }
       else {
         location->value = value;
-        delete (newNode);
+        delete toAdd;
         return;
       }
     }
-    location = newNode;
-  }
-  Node ** whereis(const K & key) {
-    Node ** location = &root;
-    /* while (*location != NULL) {
-      if ((*location)->key > key) {
-        if ((*location)->left == NULL) {
-          return location;
-        }
-        else {
-          location = &((*location)->left);
-        }
-      }
-      else if ((*location)->key < key) {
-        if ((*location)->right == NULL) {
-          return location;
-        }
-        else {
-          location = &((*location)->right);
-        }
-      }
-      else {
-        return location;
-      }
-      }*/
-    return location;
+    root = toAdd;
   }
 
   virtual const V & lookup(const K & key) const throw(std::invalid_argument) {
-    Node ** location = whereis(key);
-    if (*location == NULL) {
-      throw std::invalid_argument("The Key is not included");
+    Node * location = root;
+    while (location != NULL) {
+      if (key == location->key) {
+        return location->value;
+      }
+      else if (key < location->key) {
+        location = location->left;
+      }
+      else {
+        location = location->right;
+      }
     }
-    else {
-      return (*location)->value;
-    }
-    throw std::invalid_argument("The Key is not included");
+    throw std::invalid_argument("Not found.");
   }
 
   virtual void remove(const K & key) {
-    Node ** location = whereis(key);
-    if (*location == NULL) {
-      exit(EXIT_FAILURE);
-    }
-    if ((*location)->left == NULL && (*location)->right == NULL) {
-      delete (*location);
-      *location = NULL;
-    }
-    if ((*location)->left != NULL && (*location)->right == NULL) {
-      Node * temp = *location;
-      *location = (*location)->left;
-      delete temp;
-    }
-
-    if ((*location)->left == NULL && (*location)->right != NULL) {
-      Node * temp = *location;
-      *location = (*location)->right;
-      delete temp;
-    }
-    if ((*location)->left != NULL && (*location)->right != NULL) {
-      Node * temp = (*location)->left;
-      Node * temp_parent = temp;
-      if (temp->right == NULL) {
-        delete *location;
-        *location = temp;
-        return;
+    Node * location = root;
+    Node * P = root;
+    while (location != NULL) {
+      if (location->key < key) {
+        if (location->right == NULL) {
+          exit(EXIT_FAILURE);
+        }
+        else {
+          P = location;
+          location = location->right;
+        }
       }
-      while (temp->right != NULL) {
-        temp_parent = temp;
-        temp = temp->right;
+      else if (location->key > key) {
+        if (location->left == NULL) {
+          exit(EXIT_FAILURE);
+        }
+        else {
+          P = location;
+          location = location->left;
+        }
       }
-      temp_parent->right = temp->left;
-      temp->left = (*location)->left;
-      temp->right = (*location)->right;
-
-      delete (*location);
-      *location = temp;
-    }
-  }
-  //tbc
-  void destroy(Node * current) {
-    if (current != NULL) {
-      destroy(current->left);
-      destroy(current->right);
-      delete current;
+      else {
+        if (location->left == NULL && location->right == NULL) {
+          if (P->left == location) {
+            P->left = NULL;
+          }
+          else if (P->right == location) {
+            P->right = NULL;
+          }
+          else {
+            root = NULL;
+          }
+          delete location;
+          return;
+        }
+        else if (location->left == NULL && location->right != NULL) {
+          if (P->left == location) {
+            P->left = location->right;
+          }
+          else if (P->right == location) {
+            P->right = location->right;
+          }
+          else {
+            root = location->right;
+          }
+          delete location;
+          return;
+        }
+        else if (location->left != NULL && location->right == NULL) {
+          if (P->right == location) {
+            P->right = location->left;
+          }
+          else if (P->left == location) {
+            P->left = location->left;
+          }
+          else {
+            root = location->left;
+          }
+          delete location;
+          return;
+        }
+        else {
+          Node * temp = location->left;
+          Node * temp_P = temp;
+          while (temp->right != NULL) {
+            temp_P = temp;
+            temp = temp->right;
+          }
+          if (temp_P == temp) {
+            temp->right = location->right;
+            if (P->left == location) {
+              P->left = temp;
+            }
+            else if (P->right == location) {
+              P->right = temp;
+            }
+            else {
+              root = temp;
+            }
+          }
+          else {
+            temp_P->right = temp->left;
+            temp->left = location->left;
+            temp->right = location->right;
+            if (P->left == location) {
+              P->left = temp;
+            }
+            else if (P->right == location) {
+              P->right = temp;
+            }
+            else {
+              root = temp;
+            }
+          }
+          delete location;
+          return;
+        }
+      }
     }
   }
   virtual ~BstMap<K, V>() { destroy(root); }
-
-  void printInorder(Node * current) {
-    if (current != NULL) {
-      printInorder(current->left);
-      std::cout << current->key << " " << std::endl;
-      printInorder(current->right);
+  void destroy(Node * n) {
+    if (n != NULL) {
+      destroy(n->left);
+      destroy(n->right);
+      delete n;
+    }
+  }
+  void printInorder(Node * n) {
+    if (n != NULL) {
+      printInorder(n->left);
+      std::cout << n->key << " " << std::endl;
+      printInorder(n->right);
     }
   }
   Node * getRoot() { return root; }
