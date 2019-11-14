87037e04a17719117a756410a4421ff99769d184
zt45 <zt45@duke.edu>
2019-11-14 18:23:23 -0500

    :

--- 102_huff_tree/buildTree.cpp
+++ 102_huff_tree/buildTree.cpp
@@ -1,5 +1,32 @@
-#include "node.h"
+#include <assert.h>
 
-Node *  buildTree(uint64_t * counts) {
+#include "node.h"
+Node * buildTree(uint64_t * counts) {
   //WRITE ME!
-}  
+  assert(counts != NULL);
+  priority_queue_t qt;
+  int num = 0;
+  for (int i = 0; i < 257; i++) {
+    if (counts[i] != 0) {
+      num++;
+      qt.push(new Node(i, counts[i]));
+    }
+  }
+  if (num == 0) {
+    return NULL;
+  }
+  Node * l = NULL;
+  Node * r = NULL;
+  while (qt.size() > 1) {
+    l = qt.top();
+    qt.pop();
+
+    r = qt.top();
+    qt.pop();
+
+    qt.push(new Node(l, r));
+    l = NULL;
+    r = NULL;
+  }
+  return qt.top();
+}
